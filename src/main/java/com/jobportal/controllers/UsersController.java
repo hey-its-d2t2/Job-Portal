b741f7540aa3c889e2b75ef2406189be8282e5d7
Deepak Singh <deepsinghkumar01@gmail.com>
2024-05-24 09:28:45 +0530

    Controller: UsersController implemented and register url added.

---
+++ src/main/java/com/jobportal/controllers/UsersController.java
@@ -0,0 +1,31 @@
+package com.jobportal.controllers;
+
+import com.jobportal.entity.Users;
+import com.jobportal.entity.UsersType;
+import com.jobportal.services.UsersTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class UsersController {
+
+    private final UsersTypeService usersTypeService;
+
+    @Autowired
+    public UsersController(UsersTypeService usersTypeService) {
+        this.usersTypeService = usersTypeService;
+    }
+
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        List<UsersType> usersTypes = usersTypeService.getAll();
+        model.addAttribute("getAllTypes", usersTypes);
+        model.addAttribute("user",new Users());
+        return "register";
+    }
+}
