95f4677d40fb2778f887e9988e42511fc534a100
Deepak Singh <deepsinghkumar01@gmail.com>
2024-05-27 22:56:02 +0530

    Created : Skills java class

---
+++ src/main/java/com/jobportal/entity/Skills.java
@@ -0,0 +1,18 @@
+package com.jobportal.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "skills")
+public class Skills {
+
+}
