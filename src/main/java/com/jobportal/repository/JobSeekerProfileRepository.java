455c990fc0536ac3888503921c019ac666de92da
Deepak Singh <deepsinghkumar01@gmail.com>
2024-05-27 23:07:11 +0530

    Created : RecruiterProfileRepository Aand JobSeekerProfileRepository  java Repository

---
+++ src/main/java/com/jobportal/repository/JobSeekerProfileRepository.java
@@ -0,0 +1,7 @@
+package com.jobportal.repository;
+
+import com.jobportal.entity.JobSeekerProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobSeekerProfileRepository extends JpaRepository<JobSeekerProfile,Integer> {
+}
---
+++ src/main/java/com/jobportal/repository/RecruiterProfileRepository.java
@@ -0,0 +1,7 @@
+package com.jobportal.repository;
+
+import com.jobportal.entity.RecruiterProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruiterProfileRepository extends JpaRepository<RecruiterProfile,Integer> {
+}
