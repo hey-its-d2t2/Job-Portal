c84f2f70e71b9aed3a6927c2412ec529e3b9f387
Deepak Singh <deepsinghkumar01@gmail.com>
2024-05-28 09:12:39 +0530

    Security class added

---
+++ src/main/java/com/jobportal/config/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.jobportal.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+    private  final  String[] publicUrl = {"/",
+            "/global-search/**",
+            "/register",
+            "/register/**",
+            "/webjars/**",
+            "/resources/**",
+            "/assets/**",
+            "/css/**",
+            "/summernote/**",
+            "/js/**",
+            "/*.css",
+            "/*.js",
+            "/*.js.map",
+            "/fonts**", "/favicon.ico", "/resources/**", "/error"
+    };
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
+        http.authorizeRequests(auth -> {
+                auth.requestMatchers(publicUrl).permitAll();
+                auth.anyRequest().authenticated();
+        });
+        return  http.build();
+    }
+}
