080981f1641aec6eabe03cf23634a30e6edb3697
Deepak Singh <deepsinghkumar01@gmail.com>
2024-05-28 09:30:15 +0530

    Updated UsersRepository class.

--- src/main/java/com/jobportal/repository/UsersRepository.java
+++ src/main/java/com/jobportal/repository/UsersRepository.java
@@ -3,5 +3,9 @@ package com.jobportal.repository;
 import com.jobportal.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 public interface UsersRepository extends JpaRepository<Users,Integer> {
+    Optional<Users> findByEmail(String email);
 }
