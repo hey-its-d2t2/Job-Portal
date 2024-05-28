efd8de9bfb7c115a3e43c04a124458296658dbbc
Deepak Singh <deepsinghkumar01@gmail.com>
2024-05-28 11:58:27 +0530

    Updated RecruiterProfileService Class.

---
+++ src/main/java/com/jobportal/services/RecruiterProfileService.java
@@ -0,0 +1,22 @@
+package com.jobportal.services;
+
+import com.jobportal.entity.RecruiterProfile;
+import com.jobportal.repository.RecruiterProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RecruiterProfileService {
+    private final RecruiterProfileRepository recruiterProfileRepository;
+
+    @Autowired
+    public RecruiterProfileService(RecruiterProfileRepository recruiterProfileRepository) {
+        this.recruiterProfileRepository = recruiterProfileRepository;
+    }
+
+    public Optional<RecruiterProfile> getOne(Integer id) {
+        return recruiterProfileRepository.findById(id);
+    }
+}
