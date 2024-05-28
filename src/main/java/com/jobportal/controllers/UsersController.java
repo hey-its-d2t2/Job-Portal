262153cd180b108ca147dfc8b4dd84743848d92b
Deepak Singh <deepsinghkumar01@gmail.com>
2024-05-28 10:53:19 +0530

    Updated UsersController class

--- src/main/java/com/jobportal/controllers/UsersController.java
+++ src/main/java/com/jobportal/controllers/UsersController.java
@@ -4,8 +4,13 @@ import com.jobportal.entity.Users;
 import com.jobportal.entity.UsersType;
 import com.jobportal.services.UsersService;
 import com.jobportal.services.UsersTypeService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,5 +43,22 @@ public class UsersController {
     public String userRegistration(@Valid Users users, BindingResult result, Model model) {
        usersService.addNew(users);
         return "dashboard";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+
+        return "redirect:/";
     }
 }
