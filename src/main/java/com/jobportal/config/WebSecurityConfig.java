64daeb54b1720f6f7bfc71e3f109f4e79cdf4c11
Deepak Singh <deepsinghkumar01@gmail.com>
2024-05-28 10:35:31 +0530

    Updated WebSecurityConfig class.

--- src/main/java/com/jobportal/config/WebSecurityConfig.java
+++ src/main/java/com/jobportal/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,10 +15,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class WebSecurityConfig {
    private final CustomUserDetailsService  customUserDetailsService;
+   private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+
 
    @Autowired
-   public WebSecurityConfig(CustomUserDetailsService customUserDetailsService) {
+   public WebSecurityConfig(CustomUserDetailsService customUserDetailsService,CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler) {
        this.customUserDetailsService = customUserDetailsService;
+       this.customAuthenticationSuccessHandler = customAuthenticationSuccessHandler;
    }
 
     private  final  String[] publicUrl = {"/",
@@ -40,11 +44,21 @@ public class WebSecurityConfig {
             throws Exception {
 
         http.authenticationProvider(authenticationProvider());
-        http.authorizeRequests(auth -> {
-                auth.requestMatchers(publicUrl).permitAll();
-                auth.anyRequest().authenticated();
+
+        http.authorizeHttpRequests(auth -> {
+            auth.requestMatchers(publicUrl).permitAll();
+            auth.anyRequest().authenticated();
         });
-        return  http.build();
+
+        http.formLogin(form->form.loginPage("/login").permitAll()
+                        .successHandler(customAuthenticationSuccessHandler))
+                .logout(logout-> {
+                    logout.logoutUrl("/logout");
+                    logout.logoutSuccessUrl("/");
+                }).cors(Customizer.withDefaults())
+                .csrf(csrf->csrf.disable());
+
+        return http.build();
     }
 
     @Bean
