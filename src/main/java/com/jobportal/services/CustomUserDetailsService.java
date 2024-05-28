11ee7ae433f47a8a99aa740d562d0376cb0660a1
Deepak Singh <deepsinghkumar01@gmail.com>
2024-05-28 09:31:09 +0530

    Created CustomUserDetailsService  and CustomUserDetails classes.

---
+++ src/main/java/com/jobportal/services/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.jobportal.services;
+
+import com.jobportal.entity.Users;
+import com.jobportal.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UsersRepository usersRepository;
+    @Autowired
+    public CustomUserDetailsService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+
+        Users users = usersRepository.findByEmail(userName).orElseThrow(()->new UsernameNotFoundException("Could not find user " + userName));
+        return null;
+    }
+
+}
