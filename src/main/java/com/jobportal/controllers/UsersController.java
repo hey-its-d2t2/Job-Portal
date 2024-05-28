ffc439711841021f58da4891f125b9193c3ce519
Deepak Singh <deepsinghkumar01@gmail.com>
2024-05-28 11:58:11 +0530

    Updated UsersController Class.

--- src/main/java/com/jobportal/controllers/UsersController.java
+++ src/main/java/com/jobportal/controllers/UsersController.java
@@ -42,7 +42,7 @@ public class UsersController {
     @PostMapping("/register/new")
     public String userRegistration(@Valid Users users, BindingResult result, Model model) {
        usersService.addNew(users);
-        return "dashboard";
+        return "redirect:/dashboard/";
     }
 
     @GetMapping("/login")
