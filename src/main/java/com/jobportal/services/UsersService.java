d19fe441448c5f62355570b55f6b72c53df7eb75
Deepak Singh <deepsinghkumar01@gmail.com>
2024-05-28 10:35:43 +0530

    Updated UsersService class.

--- src/main/java/com/jobportal/services/UsersService.java
+++ src/main/java/com/jobportal/services/UsersService.java
@@ -7,6 +7,7 @@ import com.jobportal.repository.JobSeekerProfileRepository;
 import com.jobportal.repository.RecruiterProfileRepository;
 import com.jobportal.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -16,19 +17,22 @@ public class UsersService {
     private final UsersRepository usersRepository;
     private  final JobSeekerProfileRepository jobSeekerProfileRepository;
     private final RecruiterProfileRepository recruiterProfileRepository;
+    private  final PasswordEncoder passwordEncoder;
 
 
     @Autowired
     public UsersService(UsersRepository usersRepository, JobSeekerProfileRepository jobSeekerProfileRepository,
-                        RecruiterProfileRepository recruiterProfileRepository) {
+                        RecruiterProfileRepository recruiterProfileRepository,PasswordEncoder passwordEncoder) {
         this.usersRepository = usersRepository;
         this.jobSeekerProfileRepository = jobSeekerProfileRepository;
         this.recruiterProfileRepository = recruiterProfileRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Users addNew(Users users){
         users.setActive(true);
         users.setRegistrationDate(new Date(System.currentTimeMillis()));
+        users.setPassword(passwordEncoder.encode(users.getPassword()));
         Users savedUsers = usersRepository.save(users);
         int userTypeId = users.getUserTypeId().getUserTypeId();
         if(userTypeId ==1){
