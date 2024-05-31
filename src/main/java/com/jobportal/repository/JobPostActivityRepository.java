3b683243399e2492c9f2e3fd2aa9c28c70f9f278
Deepak Singh <deepsinghkumar01@gmail.com>
2024-05-31 19:06:42 +0530

    Created Repository for Job post activity..

---
+++ src/main/java/com/jobportal/repository/JobPostActivityRepository.java
@@ -0,0 +1,7 @@
+package com.jobportal.repository;
+
+import com.jobportal.entity.JobPostActivity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobPostActivityRepository  extends JpaRepository<JobPostActivity,Integer> {
+}
