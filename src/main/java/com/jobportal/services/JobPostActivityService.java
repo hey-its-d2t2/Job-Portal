b906dab1545950216b33aace3db84bceac855241
Deepak Singh <deepsinghkumar01@gmail.com>
2024-05-31 22:22:01 +0530

    Updated JobPostActivityService class for Listing jobs posted by recruiter.

--- src/main/java/com/jobportal/services/JobPostActivityService.java
+++ src/main/java/com/jobportal/services/JobPostActivityService.java
@@ -1,9 +1,12 @@
 package com.jobportal.services;
 
-import com.jobportal.entity.JobPostActivity;
+import com.jobportal.entity.*;
 import com.jobportal.repository.JobPostActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class JobPostActivityService {
@@ -17,5 +20,23 @@ public class JobPostActivityService {
 
     public JobPostActivity addNew(JobPostActivity jobPostActivity) {
         return jobPostActivityRepository.save(jobPostActivity);
+    }
+
+    public List<RecruiterJobsDto> getRecruiterJobs(int recruiter) {
+        List<IRecruiterJobs> recruiterJobsDto = jobPostActivityRepository.getRecruiterJobs(recruiter);
+
+        List<RecruiterJobsDto> recruiterJobsDtoList = new ArrayList<>();
+
+        for (IRecruiterJobs rec : recruiterJobsDto) {
+            JobLocation loc = new JobLocation(rec.getLocationId(), rec.getCity(), rec.getState(), rec.getCountry());
+            JobCompany comp = new JobCompany(rec.getCompanyId(), rec.getName(), "");
+            recruiterJobsDtoList.add(new RecruiterJobsDto(rec.getTotalCandidates(), rec.getJob_post_id(),
+                    rec.getJob_title(), loc, comp));
+        }
+        return recruiterJobsDtoList;
+    }
+    public JobPostActivity getOne(int id) {
+
+        return jobPostActivityRepository.findById(id).orElseThrow(()->new RuntimeException("Job not found"));
     }
 }
