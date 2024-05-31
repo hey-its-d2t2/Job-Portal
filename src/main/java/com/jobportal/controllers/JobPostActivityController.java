9160067970108182b80cf7a15fc40f75cf52c2ac
Deepak Singh <deepsinghkumar01@gmail.com>
2024-05-31 22:23:18 +0530

    Updated JobPostActivityController class for getting jobs details posted by recruiter.

--- src/main/java/com/jobportal/controllers/JobPostActivityController.java
+++ src/main/java/com/jobportal/controllers/JobPostActivityController.java
@@ -1,12 +1,15 @@
 package com.jobportal.controllers;
 
 import com.jobportal.entity.JobPostActivity;
+import com.jobportal.entity.RecruiterJobsDto;
+import com.jobportal.entity.RecruiterProfile;
 import com.jobportal.entity.Users;
 import com.jobportal.services.JobPostActivityService;
 import com.jobportal.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class JobPostActivityController {
@@ -34,6 +38,10 @@ public class JobPostActivityController {
         if (!(authentication instanceof AnonymousAuthenticationToken)) {
             String currentUsername = authentication.getName();
             model.addAttribute("username", currentUsername);
+            if(authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))){
+                List<RecruiterJobsDto> recruiterJobs = jobPostActivityService.getRecruiterJobs(((RecruiterProfile) currentUserProfile).getUserAccountId());
+                model.addAttribute("jobPost", recruiterJobs);
+            }
         }
 
         model.addAttribute("user", currentUserProfile);
